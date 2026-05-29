39fcaa3bfe56eebd4c44c1bd94ebeb9833aeb642
Parzival <77898157+ahmetilterinci@users.noreply.github.com>
2026-05-29 19:53:21 +0300

    db.create_all ile Render SQLite uyumu

--- run.py
+++ run.py
@@ -9,9 +9,12 @@ veya:
 
 import os
 
-from app import create_app
+from app import create_app, db
 
 app = create_app(os.environ.get("FLASK_CONFIG", "development"))
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True)
