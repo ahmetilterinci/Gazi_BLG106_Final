d98e967ea3316e7d87fc8db4840860430dd85f57
Parzival <77898157+ahmetilterinci@users.noreply.github.com>
2026-05-30 19:41:45 +0300

    PostgreSQL db.create_all fix - Oturum 5

--- run.py
+++ run.py
@@ -13,6 +13,10 @@ from app import create_app, db
 
 app = create_app(os.environ.get("FLASK_CONFIG", "development"))
 
+with app.app_context():
+    db.create_all()
+    from flask_migrate import stamp
+    stamp()
 
 if __name__ == "__main__":
     app.run(debug=True)
